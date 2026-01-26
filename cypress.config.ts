c883ec0c2761696f0dffc330095ea6dfb7d65857
ramizPro <ozbej07@gmail.com>
2026-01-26 13:24:11 +0100

    files for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // zaenkrat nič – pusti prazno
+      return config;
     },
   },
 });
